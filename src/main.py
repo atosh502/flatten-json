da3b46634bc75f98cda2b69e23e5bd647f4d7bd2
Aashutosh Poudel <aashutosh.poudel@logicinfo.com>
2020-09-07 22:39:38 +0545

    feat: add files to git

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import json
+from collections import deque
+
+def joinKeys(keys):
+  return '.'.join([str(k) for k in keys])
+
+def flatten(obj):
+  queue = deque()
+  queue.append([])
+
+  result = {}
+  while queue:
+    keys = queue.popleft()
+    prefix = joinKeys(keys)
+
+    currObj = obj
+    for key in keys:
+      currObj = currObj[key]
+
+    if isinstance(currObj, dict):
+      for k, v in currObj.items():
+        if isinstance(v, dict):
+          queue.append([*keys, k])
+        elif isinstance(v, list):
+          for idx in range(len(v)):
+            queue.append([*keys, k, idx])
+        else:
+          if prefix:
+            result[joinKeys([prefix, k])] = v
+          else:
+            result[k] = v
+    elif isinstance(currObj, list):
+      for idx in range(len(currObj)):
+        queue.append([*keys, k, idx])
+    else:
+      result[prefix] = currObj
+  return result
+
+def main():
+  with open('./sample.json') as f:
+    data = json.load(f)
+    result = flatten(data)
+    print(result)
+
+if __name__ == "__main__":
+  main()
